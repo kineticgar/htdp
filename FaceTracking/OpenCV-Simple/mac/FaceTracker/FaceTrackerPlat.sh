64cf366cc9b7e01dd9fdd954803ce9bd01cb4114
christian.muise <christian.muise@319d1e8c-3149-0410-9223-7f73c0495899>
2008-04-09 19:45:35 +0000

    Added a Platypus script to run the app.
    git-svn-id: http://htdp.googlecode.com/svn/trunk@17 319d1e8c-3149-0410-9223-7f73c0495899

---
+++ FaceTracking/OpenCV-Simple/mac/FaceTracker/FaceTrackerPlat.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# FaceTracker.sh
+# FaceTracker
+#
+# Created by Christian Muise on 25/01/08.
+#  Copyright (c) 2008 Christian Muise. All rights reserved.
+
+cd $1/../
+
+cd ./FaceTracker.app/Contents/Resources/
+./FaceTracker.sh
+cd ../../../
