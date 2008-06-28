67d873394de46528318f0d7439df13b31449ddb8
chrisnicholls1 <chrisnicholls1@319d1e8c-3149-0410-9223-7f73c0495899>
2008-06-28 02:12:19 +0000

    I've spotted the symmetry between having one wiimote track two dots at fixed distace apart and having one dot tracked by two wiimotes placed at a fixed distance apart from each other. Because of this it sufficient for the double tracker to have two instances of a single tracker and have almost no extra code.
    At the moment, it doesn't quite work as well as the previous revision 
    but it's close, and is an elegant way of doing it. I'm giong to get 
    some sleep and work out the bugs in the morning.
    
    
    git-svn-id: http://htdp.googlecode.com/svn/trunk@59 319d1e8c-3149-0410-9223-7f73c0495899

--- WiimoteTracking/PythonDriver/linux/final/CoordinateTrackers.py
+++ WiimoteTracking/PythonDriver/linux/final/CoordinateTrackers.py
@@ -94,8 +94,6 @@ class SingleCoordinateTracker( CoordinateTracker ):
 		
 		## Check its being used as a single parser...
 		assert len(xys1) == 1 == len(xys2)
-	
-		
 		x1,y1,x2,y2 = xys1[0][0],xys1[0][1],xys2[0][0],xys2[0][1]
 		## the default datum is 1023; this is what is sent if no dots are visible to the wiimote.
 		## If one dot goes off-screen, then we use the visible dot and the last known difference 
@@ -131,47 +129,29 @@ class SingleCoordinateTracker( CoordinateTracker ):
 				self.dx = self.x2-self.x1
 				self.dy = self.y2-self.y1
 class DoubleCoordinateTracker( CoordinateTracker ):	
-	distBetweenWiimotes = 100
-	scalingForZ = 1024*distBetweenWiimotes/(2*tan(pi/8))
+
+	def __init__(self):
+		self.tracker1 = SingleCoordinateTracker()
+		self.tracker2 = SingleCoordinateTracker()
+		
 	def process(self, xys1,xys2 ):
 		assert 1 < len(xys1) == len(xys2)
-		## We have data from at least two wiimotes. We'll only look at the first two for now
-		
-		## Again, it's hard to explain the following code without a diagram.
-		## We're assuming that two wiimotes are parallel and the ditance between them is
-		## distBetweenWiimotes
-		## The larger this value, the more accurate the calculations will be, but the
-		## field of view will be smaller. 	
-		
+
 		x1,x2,= xys1[0][0],xys1[1][0] ## the first x coordinate from each remote
 		x3,x4 = xys2[0][0],xys2[1][0] ## the second x coordinate from each remote
 		y1,y2,= xys1[0][1],xys1[1][1] ## the first y coordinate from each remote
 		y3,y4 = xys2[0][1],xys2[1][1] ## the second y coordinate from each remote
-		## for the moment we won't deal with incomplete data.
-		if 1023 in (x1,x2,x3,x4):
-			return
-		## we need to check thet the first dot from each remote  reffers to the same led. 
-		## We'll do this by looking at the dot product of the the vectors defined 
-		## by each pair of points.
-		if (x3-x1)*(x4-x2)+(y3-y1)*(y4-y2)<0:	 
+ 		## we need to check thet the first dot from each remote  reffers to the same led. 
+        ## We'll do this by looking at the dot product of the the vectors defined 
+        ## by each pair of points.
+		if (x3-x1)*(x4-x2)+(y3-y1)*(y4-y2)<0:    
 			x2,x4,y2,y4 = x4,x2,y2,y4
-			
-		## Now we can do the psudo triangulation. 
-		if not 1023 in (x1,x2):
-			a = math.sqrt((x2-x1)**2+(y2-y1)**2)
-			if a != 0:
-				self.z1 = self.scalingForZ/a
-				self.x1 = self.distBetweenWiimotes*(x1-512)/a
-				self.y1 = self.distBetweenWiimotes*(y1-300)/a
-		if not 1023 in (x3,x4):
-			b = math.sqrt((x4-x3)**2+(y4-y3)**2)
-			if b !=0:
-	 			self.z2 = self.scalingForZ/b	
-	 			self.x2 = self.distBetweenWiimotes*(x3-512)/b
-	 			self.y2 = self.distBetweenWiimotes*(y3-300)/b
-	 	self.size = math.sqrt((self.x1-self.x2)**2+(self.y1-self.y2)**2+(self.z1-self.z2)**2)
-	 	print self.size
-	 	#print 400<self.size<500, x1,x2,x3,x4,y1,y2,y3,y4,self.size
+                
+		self.tracker1.process([(x1,y1)],[(x2,y2)])
+		self.tracker2.process([(x3,y3)],[(x4,y4)])
+		self.x1,self.y1,self.z1= self.tracker1.getCoordinates()[0]
+		self.x2,self.y2,self.z2 = self.tracker2.getCoordinates()[0]
+		
 
 def CoordinateTrackerFactory(n,correctErrors = True):
 	if n <= 0: 
