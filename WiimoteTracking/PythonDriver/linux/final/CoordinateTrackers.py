fb5967f06ca06a708b12ba66db0b40c694f361c4
chrisnicholls1 <chrisnicholls1@319d1e8c-3149-0410-9223-7f73c0495899>
2008-07-07 13:45:46 +0000

    A slightly more relaible version of coordinateTracker. It has a handfull of print statements which I will remove once I'm happy with its performance.
    git-svn-id: http://htdp.googlecode.com/svn/trunk@61 319d1e8c-3149-0410-9223-7f73c0495899

--- WiimoteTracking/PythonDriver/linux/final/CoordinateTrackers.py
+++ WiimoteTracking/PythonDriver/linux/final/CoordinateTrackers.py
@@ -65,10 +65,13 @@ class CoordinateTracker:
 		self.dz  = 0
 		self.buttonA = 0
 
-	def size(self):
+	def length(self):
 		return math.sqrt((self.x1-self.x2)**2+(self.y1-self.y2)**2+(self.z1-self.z2)**2)
 	def getCoordinates(self):
-		return (self.x1,self.y1,self.z1), (self.x2,self.y2,self.z2)
+		return (self.x1,self.y1,self.z1), (
+				self.x1 + self.dx,
+				self.y1 + self.dy,
+				self.z1 + self.dz)
 		
 	def correct(self,v1,v2,old1,old2,dv,factor,shift =0):
 			if v1==1023 and v2 == 1023:
@@ -124,12 +127,26 @@ class SingleCoordinateTracker( CoordinateTracker ):
 			self.y1,self.y2,self.dy = self.correct(y1,y2,self.y1,self.y2,self.dy,factor,shifty)
 			
 		#print self.size()	
+import pygame		
+class Dots:
+	def __init__(self,n = 10,colour =(0, 0, 0) ):
+		
+		self.bgcolor = 255, 255, 255
+		self.linecolor = colour
+		self.screen = pygame.display.set_mode((1000, 800))
+		pygame.draw.circle(self.screen, self.linecolor, (10,10), 30)
+		self.screen.fill(self.bgcolor)
+	 	pygame.display.flip()
+	def draw(self,x1,y1,c):
+		pygame.draw.circle(self.screen,c,(x1, y1),2)	
+		pygame.display.flip()
 class DoubleCoordinateTracker( CoordinateTracker ):	
-
-
+	old =0,0
+	#graph = Dots()
 	tracker1 = SingleCoordinateTracker()
 	tracker2 = SingleCoordinateTracker()
-	oldx = 0
+	switch = False
+	visible = 15
 	def process(self, xys1,xys2 ):
 		assert 1 < len(xys1) == len(xys2)
 
@@ -141,19 +158,39 @@ class DoubleCoordinateTracker( CoordinateTracker ):
 		## We'll do this by looking at the dot product of the the vectors defined 
 		## by each pair of points.
 
-		
+		visible = reduce((lambda x,y: (x << 1) + (y!=1023)),(x1,x2,x3,x4),0)
+		if visible == 15: self.visible = 15
+		else: self.visible &=visible
 		if 1023 not in (x1,x2,x3,x4):
 			if (x3-x1)*(x4-x2)+(y3-y1)*(y4-y2)<0:    
-					x2,x4,y2,y4 = x4,x2,y2,y4
-		self.tracker1.process([(x1,y1)],[(x2,y2)])	                
-		self.tracker2.process([(x3,y3)],[(x4,y4)])
+					self.switch = True
+										
+			else: 
+				self.switch = False
+		for x in(x1,x2,x3,x4,y1,y2,y3,y4):
+			if x ==1023: print '###',
+			else: print  '%03i' %x,
 			
-		self.x1,self.y1,self.z1 = self.tracker1.getCoordinates()[0]
-		self.x2,self.y2,self.z2 = self.tracker2.getCoordinates()[0]
-		self.dx = self.x2 - self.x1
-		self.dy = self.y2 - self.y1	
-		self.dz = self.z2 - self.z1	
+		if self.switch:
+			x2,x4,y2,y4 = x4,x2,y4,y2
+			print 's',
+		else: print '~',
 
+		
+		if not self.visible in (0,1,2,4,8):
+			self.tracker1.process([(x1,y1)],[(x2,y2)])	                
+			self.tracker2.process([(x3,y3)],[(x4,y4)])
+				
+			self.x1,self.y1,self.z1 = self.tracker1.getCoordinates()[0]
+			self.x2,self.y2,self.z2 = self.tracker2.getCoordinates()[0]
+			self.dx = self.x2 - self.x1
+			self.dy = self.y2 - self.y1	
+			self.dz = self.z2 - self.z1	
+		print "%04i" % self.length(),
+		if 160< self.length() < 240:
+			print '|||'
+		else:
+			print '<<<'
 
 
 def CoordinateTrackerFactory(n,correctErrors = True):
